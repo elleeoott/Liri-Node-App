d29b26b527910f40b626416bc80cbd296398e161
Evan Darst <evan.l.darst@gmail.com>
2018-01-09 22:42:28 -0500

    created function for searching for tweets based on username

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+import { error } from "util";
+
 require("dotenv").config();
 
 //Creating variables to require the installed packages and access keys
@@ -15,3 +17,34 @@ var spotify = new Spotify(keys.spotify);
 //Calls on Twitter key
 
 var client = new Twitter(keys.twitter);
+
+//Search function that finds tweets from Twitter account
+
+var getTwitter = function() {
+
+    //Accesses the Twitter key
+
+    var client = new Twitter(keys.twitter);
+
+    var params = {screen_name: "evanldarst", count: 10};
+    client.get("statuses/user_timeline", params, function(error, tweets, response)) {
+
+        if (error) {
+
+            throw error
+        }
+
+        console.log(tweets[0].text);
+        for (var i = 0; i < tweets.length; i++) {
+            
+            console.log(this);
+        }
+    }
+
+
+}
+
+//Search function that finds a song in Spotify
+
+
+//Run the method
