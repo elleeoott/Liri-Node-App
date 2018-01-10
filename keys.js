29b8d2d59c50b0818cc8ee76c8a6a5968016855a
Evan Darst <evan.l.darst@gmail.com>
2018-01-09 20:26:22 -0500

    created Twitter access keys for keys.js

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.PoYROUrMrEyUNOiDVRJoBrVGg,
+  consumer_secret: process.env.	WbliwBHRNxhh1ZWoORks5iAAxj0o9T5Yai1nGlpIDJo3CsktJX
+,
+  access_token_key: process.env.904835451797868544-SGs9atBpkQebjWfXNVYraKTi5oRlNj7,
+  access_token_secret: process.env.10W7Mis7Bp8VC6fFP5EAotCa9MohsmhbdHMNO1msLCCjU
 };
 
 exports.spotify = {
