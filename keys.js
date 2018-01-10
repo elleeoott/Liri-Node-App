5821792ce53369623b1fa45329dc307a604c1a52
Evan Darst <evan.l.darst@gmail.com>
2018-01-09 20:29:25 -0500

    added Spotify access keys in keys.js

--- keys.js
+++ keys.js
@@ -2,13 +2,13 @@ console.log('this is loaded');
 
 exports.twitter = {
   consumer_key: process.env.PoYROUrMrEyUNOiDVRJoBrVGg,
-  consumer_secret: process.env.	WbliwBHRNxhh1ZWoORks5iAAxj0o9T5Yai1nGlpIDJo3CsktJX
+  consumer_secret: process.env.WbliwBHRNxhh1ZWoORks5iAAxj0o9T5Yai1nGlpIDJo3CsktJX
 ,
   access_token_key: process.env.904835451797868544-SGs9atBpkQebjWfXNVYraKTi5oRlNj7,
   access_token_secret: process.env.10W7Mis7Bp8VC6fFP5EAotCa9MohsmhbdHMNO1msLCCjU
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.f52c8737b07b408cb9fb79fc2f67d54d,
+  secret: process.env.ada693fe06874116998513e5bc4d62ff
 };
