348743b1326bf4dd310079b232ff62e14a7ff814
Evan Darst <evan.l.darst@gmail.com>
2018-01-09 20:45:03 -0500

    created variables to call on Spotify and Twitter keys

--- liri.js
+++ liri.js
@@ -1,1 +1,17 @@
 require("dotenv").config();
+
+//Creating variables to require the installed packages and access keys
+
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var keys = require("./keys");
+var fs = require("fs");
+
+//Calls on Spotify key
+
+var spotify = new Spotify(keys.spotify);
+
+//Calls on Twitter key
+
+var client = new Twitter(keys.twitter);
